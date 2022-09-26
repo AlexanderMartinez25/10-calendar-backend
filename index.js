15a9c7861f48fd7a180cafeefdbc6b80b8947aab
Alexander <alexander.martinez@lapatota.cl>
2022-09-26 20:59:49 -0300

    Configurando Express

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("holamundo");
+const express = require("express");
+
+// Crear servidor express
+const app = express();
+
+// Rutas
+app.get("/", (req, res) => {
+  res.json({
+    ok: true,
+  });
+});
+
+// Escuchar peticiones
+app.listen(4001, () => {
+  console.log(`Servidor corriendo en pruerto ${4000}`);
+});
