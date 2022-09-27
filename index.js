e8e36899c89a6a2ec43b30dd4ee6108a2c8ae5e0
Alexander <alexander.martinez@lapatota.cl>
2022-09-26 21:10:41 -0300

    Variables de entorno y carpeta pública

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require("express");
+require("dotenv").config();
 
 // Crear servidor express
 const app = express();
 
+// Directorio Público
+app.use(express.static("public"));
+
 // Rutas
-app.get("/", (req, res) => {
-  res.json({
-    ok: true,
-  });
-});
+// app.get("/", (req, res) => {
+//   res.json({
+//     ok: true,
+//   });
+// });
 
 // Escuchar peticiones
-app.listen(4001, () => {
-  console.log(`Servidor corriendo en pruerto ${4000}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor corriendo en pruerto ${process.env.PORT}`);
 });
