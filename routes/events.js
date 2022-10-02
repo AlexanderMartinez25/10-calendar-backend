aec9bcb94322be90caecfe854f7a71a5a09d7e6c
Alexander <alexander.martinez@lapatota.cl>
2022-10-02 16:01:48 -0300

    Validar campos necesarios

--- routes/events.js
+++ routes/events.js
@@ -11,6 +11,9 @@ const {
 } = require("../controllers/events");
 const { validarJWT } = require("../middlewares/validar-jwt");
 const { Router } = require("express");
+const { check } = require("express-validator");
+const { validarCampos } = require("../middlewares/validar-campos");
+const { isDate } = require("../helpers/isDate");
 const router = Router();
 
 // Todas las peticiones tienen que pasar por la validación de JWT
@@ -20,7 +23,16 @@ router.use(validarJWT);
 router.get("/", getEventos);
 
 // Crear un nuevo evento
-router.post("/", crearEvento);
+router.post(
+  "/",
+  [
+    check("title", "El título es obligatorio").not().isEmpty(),
+    check("start", "Fecha de inicio es obligatoria").custom(isDate),
+    check("end", "Fecha de finalización es obligatoria").custom(isDate),
+    validarCampos,
+  ],
+  crearEvento
+);
 
 // Actualizar evento
 router.put("/:id", actualizarEvento);
