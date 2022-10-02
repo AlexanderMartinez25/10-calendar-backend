99a5dc5c17de378b10ae4423c54086d721e8fa48
Alexander <alexander.martinez@lapatota.cl>
2022-10-02 16:24:09 -0300

    Grabar el evento en la base de datos

--- controllers/events.js
+++ controllers/events.js
@@ -1,4 +1,5 @@
 const { response } = require("express");
+const Evento = require("../models/Evento");
 
 const getEventos = async (req, res = response) => {
   return res.status(201).json({
@@ -8,12 +9,26 @@ const getEventos = async (req, res = response) => {
 };
 
 const crearEvento = async (req, res = response) => {
-  //verificar que tenga el evento.
-  console.log(req.body);
-  return res.status(201).json({
-    ok: true,
-    msg: "crearEvento",
-  });
+  const evento = new Evento(req.body);
+
+  try {
+    // el id del usuario ya viene por defecto en la req
+    // asi que se lo asignamos al evento
+    evento.user = req.uid;
+
+    const eventoGuardado = await evento.save();
+
+    res.json({
+      ok: true,
+      evento: eventoGuardado,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Hable con el administrador",
+    });
+  }
 };
 
 const actualizarEvento = async (req, res = response) => {
--- models/Evento.js
+++ models/Evento.js
@@ -3,23 +3,33 @@ const { Schema, model } = require("mongoose");
 const EventoSchema = Schema({
   title: {
     type: String,
-    require: true,
+    required: true,
   },
   notes: {
     type: String,
   },
   start: {
     type: Date,
-    require: true,
+    required: true,
   },
   end: {
     type: Date,
-    require: true,
+    required: true,
   },
+  // lareferencia al modelo usuario con su id
   user: {
     type: Schema.Types.ObjectId,
     ref: "Usuario",
+    required: true,
   },
+});
+
+// modificar lo que devuelve el Schema (_id) por id y ("val")
+EventoSchema.method("toJSON", function () {
+  const { __V, _id, ...object } = this.Object();
+
+  object.id = _id;
+  return object;
 });
 
 module.exports = model("Evento", EventoSchema);
