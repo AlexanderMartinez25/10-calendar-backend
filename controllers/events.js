e64043ef6e197530a9784c66b5350b73e2e622eb
Alexander <alexander.martinez@lapatota.cl>
2022-10-03 08:17:19 -0300

    Actualizar un Evento

--- controllers/events.js
+++ controllers/events.js
@@ -34,13 +34,50 @@ const crearEvento = async (req, res = response) => {
 };
 
 const actualizarEvento = async (req, res = response) => {
-  const { id } = req.params;
-  console.log(id);
+  const eventId = req.params.id;
+  const uid = req.uid;
 
-  return res.status(201).json({
-    ok: true,
-    msg: "actualizarEvento",
-  });
+  try {
+    const evento = await Evento.findById(eventId);
+
+    if (!evento) {
+      res.status(404).json({
+        ok: false,
+        msg: "Evento no existe por ese id",
+      });
+    }
+
+    // si un usuario quiere modificar la nota de otro usuario
+    if (evento.user.toString() !== uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: "no tiene privilegio de editar este evento",
+      });
+    }
+
+    const nuevoEvento = {
+      ...req.body,
+      user: uid,
+    };
+
+    const eventoActualizado = await Evento.findByIdAndUpdate(
+      eventId,
+      nuevoEvento,
+      // para que retorne los cambios actualizados
+      { new: true }
+    );
+
+    res.json({
+      ok: true,
+      evento: eventoActualizado,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Hable con el administrador",
+    });
+  }
 };
 
 const eliminarEvento = async (req, res = response) => {
